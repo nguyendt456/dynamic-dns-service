4eb71b4d19cc7c9e54ca52f3ce902842dfda0bd2
bronzewhale <nguyendt456@gmail.com>
2023-05-22 22:06:40 +0700

    Fix shell script

--- test/replace_credentials.sh
+++ test/replace_credentials.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sed -i "s/username:/username: ${DDNS_USERNAME}/g" ./config_test.yaml
-sed -i "s/password:/password: ${DDNS_PASSWORD}/g" ./config_test.yaml
+BASEDIR=$(dirname "$0")
+sed -i "s/username:/username: ${DDNS_USERNAME}/g" ${BASEDIR}/config_test.yaml
+sed -i "s/password:/password: ${DDNS_PASSWORD}/g" ${BASEDIR}/config_test.yaml
